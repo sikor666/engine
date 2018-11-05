3ad2f462612553bcabf63a0d4e730c718b431a0e
Tomek <sikor666@gmail.com>
2018-11-05 15:07:35 +0100

    Add tessellation and geometry shader

--- src/engine.cpp
+++ src/engine.cpp
@@ -5,8 +5,21 @@
 #include <SDL.h>
 #include <glad.h>
 
+// SDLK_ESCAPE: exit program
+// SDLK_t: trójkąt z teselacją
+// SDLK_g: trójkąt po teselacji i dodaniu shadera geometrii
+// SDLK_b: powrót do stanu początkowego
+struct KeyboardController
+{
+public:
+    void keyPress(SDL_Keycode k) { key = k; }
+    bool isPress(SDL_Keycode k) { return k == key; }
+
+private:
+    SDL_Keycode key = SDLK_b;
+} keyboard;
+
 // Window management
-bool quit = false; // exit program
 SDL_Window *window = nullptr;
 SDL_GLContext glContext;
 SDL_Event event;
@@ -75,13 +88,16 @@ GLuint compile_shaders(void)
 {
     //GLuint vertex_shader_point;
     GLuint vertex_shader_triangle;
+    GLuint tessellation_control_shader;
+    GLuint tessellation_evaluation_shader;
+    GLuint geometry_shader;
     GLuint fragment_shader;
     GLuint program;
 
     // Kod źródłowy shadera wierzchołków dla pojedynczego punktu.
     static const GLchar * vertex_shader_source_point[] =
     {
-    "#version 450 core                              \n"
+    "#version 430 core                              \n"
     "                                               \n"
     "void main(void)                                \n"
     "{                                              \n"
@@ -92,7 +108,7 @@ GLuint compile_shaders(void)
     // Kod źródłowy shadera wierzchołków dla trójkąta.
     static const GLchar * vertex_shader_source_triangle[] =
     {
-    "#version 450 core                                                          \n"
+    "#version 430 core                                                          \n"
     "                                                                           \n"
     "// ′offset′ i ′color′ to wejściowe atrybuty wierzchołka.                   \n"
     "layout (location = 0) in vec4 offset;                                      \n"
@@ -120,10 +136,60 @@ GLuint compile_shaders(void)
     "}                                                                          \n"
     };
 
+    // Kod źródłowy shadera sterowania teselacją.
+    static const GLchar * tessellation_control_shader_source[] =
+    {
+    "#version 430 core\n"
+    "layout (vertices = 3) out;\n"
+    "void main(void)\n"
+    "{\n"
+    "   // Tylko, jeśli to wywołanie o identyfikatorze 0…\n"
+    "   if (gl_InvocationID == 0)\n"
+    "   {\n"
+    "       gl_TessLevelInner[0] = 5.0;\n"
+    "       gl_TessLevelOuter[0] = 5.0;\n"
+    "       gl_TessLevelOuter[1] = 5.0;\n"
+    "       gl_TessLevelOuter[2] = 5.0;\n"
+    "   }\n"
+    "   // Zawsze kopiuj wejście na wyjście.\n"
+    "   gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;\n"
+    "}\n"
+    };
+
+    // Kod źródłowy shadera wyliczenia teselacji.
+    static const GLchar * tessellation_evaluation_shader_source[] =
+    {
+    "#version 430 core\n"
+    "layout (triangles, equal_spacing, cw) in;\n"
+    "void main(void)\n"
+    "{\n"
+    "   gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position +\n"
+    "                  gl_TessCoord.y * gl_in[1].gl_Position +\n"
+    "                  gl_TessCoord.z * gl_in[2].gl_Position);\n"
+    "}\n"
+    };
+
+    // Kod źródłowy shadera geometrii.
+    static const GLchar * geometry_shader_source[] =
+    {
+    "#version 430 core\n"
+    "layout (triangles) in;\n"
+    "layout (points, max_vertices = 3) out;\n"
+    "void main(void)\n"
+    "{\n"
+    "   int i;\n"
+    "   for (i = 0; i < gl_in.length(); i++)\n"
+    "   {\n"
+    "       gl_Position = gl_in[i].gl_Position;\n"
+    "       EmitVertex();\n"
+    "   }\n"
+    "}\n"
+    };
+
     // Kod źródłowy shadera fragmentów.
     static const GLchar * fragment_shader_source[] =
     {
-    "#version 450 core                                                              \n"
+    "#version 430 core                                                              \n"
     "                                                                               \n"
     "// Deklaracja VS_OUT jako wejściowy blok interfejsu.                           \n"
     "in VS_OUT                                                                      \n"
@@ -155,6 +221,21 @@ GLuint compile_shaders(void)
     glShaderSource(vertex_shader_triangle, 1, vertex_shader_source_triangle, NULL);
     glCompileShader(vertex_shader_triangle);
 
+    // Utworzenie i kompilacja shadera sterowania teselacją.
+    tessellation_control_shader = glCreateShader(GL_TESS_CONTROL_SHADER);
+    glShaderSource(tessellation_control_shader, 1, tessellation_control_shader_source, NULL);
+    glCompileShader(tessellation_control_shader);
+
+    // Utworzenie i kompilacja shadera wyliczenia teselacji.
+    tessellation_evaluation_shader = glCreateShader(GL_TESS_EVALUATION_SHADER);
+    glShaderSource(tessellation_evaluation_shader, 1, tessellation_evaluation_shader_source, NULL);
+    glCompileShader(tessellation_evaluation_shader);
+
+    // Utworzenie i kompilacja shadera geometrii.
+    geometry_shader = glCreateShader(GL_GEOMETRY_SHADER);
+    glShaderSource(geometry_shader, 1, geometry_shader_source, NULL);
+    glCompileShader(geometry_shader);
+
     // Utworzenie i kompilacja shadera fragmentów.
     fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
@@ -163,10 +244,22 @@ GLuint compile_shaders(void)
     // Utworzenie programu, dodanie shaderów i ich połączenie.
     // tworzy obiekt programu, do którego można dołączyć obiekty shaderów;
     program = glCreateProgram();
+
     // dołącza obiekt shadera do obiektu programu;
     // glAttachShader(program, vertex_shader_point);
     glAttachShader(program, vertex_shader_triangle);
+
+    if (!keyboard.isPress(SDLK_b) && (keyboard.isPress(SDLK_t) || keyboard.isPress(SDLK_g)))
+    {
+        glAttachShader(program, tessellation_control_shader);
+        glAttachShader(program, tessellation_evaluation_shader);
+
+        if (keyboard.isPress(SDLK_g))
+            glAttachShader(program, geometry_shader);
+    }
+
     glAttachShader(program, fragment_shader);
+
     // łączy w jedną całość wszystkie dodane obiekty shaderów;
     glLinkProgram(program);
 
@@ -175,6 +268,9 @@ GLuint compile_shaders(void)
     // zawiera kod binarny i sam shader nie jest już potrzebny.
     // glDeleteShader(vertex_shader_point);
     glDeleteShader(vertex_shader_triangle);
+    glDeleteShader(tessellation_control_shader);
+    glDeleteShader(tessellation_evaluation_shader);
+    glDeleteShader(geometry_shader);
     glDeleteShader(fragment_shader);
 
     return program;
@@ -183,6 +279,13 @@ GLuint compile_shaders(void)
 void engine::startup()
 {
     rendering_program = compile_shaders();
+
+    // Teselacja w OpenGL działa poprzez rozbicie powierzchni wysokiego poziomu nazywanych płatami
+    // (ang.patch) na punkty, linie i trójkąty.Każdy płat składa się z pewnej liczby punktów sterujących
+    // (ang.control points).Ich liczbę konfiguruje się, wywołując funkcję glPatchParameteri() z 
+    // parametrem pname ustawionym na GL_PATCH_VERTICES i parametrem value ustawionym na liczbę
+    // punktów mających tworzyć płat.
+    // glPatchParameteri(GL_PATCH_VERTICES, 3);
 
     // Przed narysowaniem czegokolwiek trzeba jeszcze
     // utworzyć tak zwany VAO (ang.Vertex Array Object),
@@ -226,15 +329,22 @@ void engine::render(double currentTime)
 
     // Funkcja ustawia średnicę punktu w pikselach na zadany rozmiar.
     // OpenGL gwarantuje obsługę przynajmniej rozmiaru 64 piksele.
-    // glPointSize(64.0f);
+    glPointSize(6.9f);
 
-    // Narysowanie jednego punktu.
+    // Aby zobaczyć wynik działania mechanizmu teselacji, trzeba poinformować OpenGL, żeby rysował
+    // jedynie zarysy wynikowych trójkątów.
+    if (!keyboard.isPress(SDLK_b)) glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    else glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
     // Funkcja glDrawArrays() wysyła wierzchołki do potoku OpenGL.
     // Dla każdego wierzchołka zostanie wykonany shader wierzchołka. 
+
+    // Narysowanie jednego punktu.
     // glDrawArrays(GL_POINTS, 0, 1);
 
     // Narysowanie trójkąta
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    if (!keyboard.isPress(SDLK_b)) glDrawArrays(GL_PATCHES, 0, 3);
+    else glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 int engine::run()
@@ -244,7 +354,7 @@ int engine::run()
     startup();
 
     // Main loop
-    while (!quit)
+    while (!keyboard.isPress(SDLK_ESCAPE))
     {
         double sec = SDL_GetTicks() / 1000.0f;
         render(sec);
@@ -257,13 +367,25 @@ int engine::run()
             switch (event.type)
             {
             case SDL_QUIT:
-                quit = true;
+                keyboard.keyPress(SDLK_ESCAPE);
                 break;
             case SDL_KEYDOWN:
                 switch (event.key.keysym.sym)
                 {
                 case SDLK_ESCAPE:
-                    quit = true;
+                    keyboard.keyPress(SDLK_ESCAPE);
+                    break;
+                case SDLK_t:
+                    keyboard.keyPress(SDLK_t);
+                    rendering_program = compile_shaders();
+                    break;
+                case SDLK_g:
+                    keyboard.keyPress(SDLK_g);
+                    rendering_program = compile_shaders();
+                    break;
+                case SDLK_b:
+                    keyboard.keyPress(SDLK_b);
+                    rendering_program = compile_shaders();
                     break;
                 default:
                     break;
