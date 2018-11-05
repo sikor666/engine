15786f1e43158fa0a200fd8df5170cb0aaed0dea
Tomek <sikor666@gmail.com>
2018-11-05 02:38:58 +0100

    Clearing window with color

--- src/engine.cpp
+++ src/engine.cpp
@@ -19,17 +19,6 @@ short unsigned int wWidth = 800;
 short unsigned int initialPosX = 100;
 short unsigned int initialPosY = 100;
 
-// OpenGL
-GLuint shaderProgram;
-
-GLuint VAO; // vertex array object
-GLuint VBO; // vertex buffer object
-
-GLuint vertices = 3;
-GLfloat vVertices[] = { 0.0f, -0.5f, 0.0f,
-                       -0.5f, 0.5f, 0.0f,
-                        0.5f, 0.5f, 0.0f };
-
 void _sdlError(const char *mes)
 {
     fprintf(stderr, "%s: %s\n", mes, SDL_GetError());
@@ -72,125 +61,34 @@ void Init()
 
     // V-Sync
     SDL_GL_SetSwapInterval(1);
-
-    // Disable depth test and face culling.
-    glDisable(GL_DEPTH_TEST);
-    glDisable(GL_CULL_FACE);
 }
 
 void PreLoop()
 {
-    // Check OpenGL properties
-    printf("Vendor:          %s\n", glGetString(GL_VENDOR));
-    printf("Renderer:        %s\n", glGetString(GL_RENDERER));
-    printf("Version OpenGL:  %s\n", glGetString(GL_VERSION));
-    printf("Version GLSL:    %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
-}
-
-void LoadShaders()
-{
-    // Shaders
-    const char* vertexShaderSource = "#version 450 core\n"
-        "layout (location = 0) in vec3 position;\n"
-        "void main()\n"
-        "{\n"
-        "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-        "}\0";
-    const char* fragmentShaderSource = "#version 450 core\n"
-        "out vec4 color;\n"
-        "void main()\n"
-        "{\n"
-        "color = vec4(0.8f, 0.6f, 0.9f, 1.0f);\n"
-        "}\n\0";
-
-    // Build compile VERTEX_SHADER
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-
-    // Check for compile errors VERTEX_SHADER
-    GLint success;
-    GLchar infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, sizeof(infoLog), nullptr, infoLog);
-        printf("Error: vertex shader compilation error: %s\n", infoLog);
-    }
-
-    // Build compile FRAGMENT_SHADER
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-
-    // Check for compile errors FRAGMENT_SHADER
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, sizeof(infoLog), nullptr, infoLog);
-        printf("Error: fragment shader compilation error: %s\n", infoLog);
-    }
-
-    // Link shaders
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    // glBindFragDataLocation(shaderProgram, 0, "color");
-    glLinkProgram(shaderProgram);
-
-    // Check the linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram, sizeof(infoLog), nullptr, infoLog);
-        printf("Error: linking error: %s\n", infoLog);
-    }
-
-    // Link shaders with application
-    GLuint position = 0;
-    glGetAttribLocation(shaderProgram, "position");
-    glEnableVertexAttribArray(position);
-    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
-
-    // Delete shaders
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
 }
 
 void OpenGLSet() // set up OpenGL
 {
     GLint vpWidth, vpHeight;
     SDL_GL_GetDrawableSize(window, &vpWidth, &vpHeight);
-    glViewport(0, 0, vpWidth, vpHeight);
-
-    LoadShaders();
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // Bind the Vertex Array Object first, then bind and set Vertex Buffers and attribute pointers
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vVertices), vVertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // ?
-    glBindVertexArray(0);
-
 }
 
 void DeleteGLTrash()
 {
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
 }
 
 void DeleteSDLTrash()
 {
     SDL_GL_DeleteContext(glContext);
     SDL_DestroyWindow(window);
+}
+
+// Funkcja renderuj¹ca
+void engine::render(unsigned __int32 currentTime)
+{
+    // Wyczyszczenie okna kolorem
+    static const GLfloat red[] = { 0.3f, 0.4f, 0.7f, 1.0f };
+    glClearBufferfv(GL_COLOR, 0, red);
 }
 
 int engine::run()
@@ -204,14 +102,7 @@ int engine::run()
     // Main loop
     while (!quit)
     {
-        glClearColor(0.6f, 0.8f, 0.9f, 1.0f); // Clear the color buffer
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Draw functions
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, vertices);
-        glBindVertexArray(0);
+        render(SDL_GetTicks());
 
         SDL_GL_SwapWindow(window); // swap buffers
         while (SDL_PollEvent(&event)) // handle events
@@ -226,12 +117,6 @@ int engine::run()
                 {
                 case SDLK_ESCAPE:
                     quit = true;
-                    break;
-                case SDLK_r:
-                    break;
-                case SDLK_g:
-                    break;
-                case SDLK_b:
                     break;
                 default:
                     break;
