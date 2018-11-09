b236e123ffa61e8c26d2f019b7df875ca30be1ad
Tomek <sikor666@gmail.com>
2018-11-09 15:30:46 +0100

    Move buffers examples to separate classes

--- media/shaders/engine.2.vs.glsl
+++ media/shaders/engine.2.vs.glsl
@@ -2,6 +2,15 @@
 #pragma debug(on)
 #pragma optimize(off)
 
+layout (std140) uniform TransformBlock
+{
+    float scale;            // globalna skala stosowana do wszystkiego
+    vec4 translation;       // przesunięcie na osiach X, Y i Z
+    float rotation[3];      // obrót wokół osi X, Y i Z
+    mat4 projection_matrix; // uogólniona macierz rzutowania stosowana
+                            // po skalowaniu i obrocie
+} transform;
+
 // position, offset i color to wejściowe atrybuty wierzchołka.
 layout (location = 0) in vec4 position;
 layout (location = 1) in vec4 offset;
@@ -12,15 +21,15 @@ out vec4 vs_color;
 
 void main(void)
 {
-   //     const vec4 colors[] = vec4[3](vec4(1.0, 0.0, 0.0, 1.0),
-   //                                   vec4(0.0, 1.0, 0.0, 1.0),
-   //                                   vec4(0.0, 0.0, 1.0, 1.0));
+    const vec4 colors[] = vec4[3](vec4(1.0, 0.0, 0.0, 1.0),
+                                  vec4(0.0, 1.0, 0.0, 1.0),
+                                  vec4(0.0, 0.0, 1.0, 1.0));
 
    // Dodaj ′offset′ do umieszczonej na sztywno pozycji.
    gl_Position = position + offset;
 
    // Przekazanie do vs_color stałej wartości.
    // Wykorzystanie indeksu gl_VertexID.
-   // vs_color = colors[gl_VertexID] + color * 0.6;
-   vs_color = color;
+   vs_color = colors[gl_VertexID] + color * 0.6;
+   // vs_color = color;
 }
--- src/buffers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <glad.h>
-
-void read_position_buffer();
-void read_named_buffers(GLuint program, GLuint vao);
-void interleaved_attributes(GLuint program, GLuint vao);
-void fillUniformBlock(GLuint program);
-void printUniformBlocks(GLuint program);
